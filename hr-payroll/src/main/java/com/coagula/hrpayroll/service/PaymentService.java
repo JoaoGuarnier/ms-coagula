5f8f77222a4cc0bb35ff6bf91dab2fd0af37f4a6
JoaoGuarnier <joaocarlosguarnier@hotmail.com>
2022-03-28 15:33:06 -0300

    rest template

--- hr-payroll/src/main/java/com/coagula/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/coagula/hrpayroll/service/PaymentService.java
@@ -1,16 +1,38 @@
 package com.coagula.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.coagula.hrpayroll.model.Payment;
+import com.coagula.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 	
 	public Payment getPayment(Long workerId, Integer days) {
-		Payment payment = new Payment("Lebrao",Double.valueOf(200), days);
+		
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+		
+		String urlhrworker = workerHost + "/workers/{id}";
+		
+		Worker worker = restTemplate.getForObject(urlhrworker, Worker.class, uriVariables);
+		
+		Payment payment = new Payment(worker.getName(),Double.valueOf(worker.getDailyIncome()), days);
 		return payment;
 	}
+	
+	
 	
 }
